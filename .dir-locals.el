852f1eede79c6726910fe28646436bdc141cdb44
Jack Feser <feser@csail.mit.edu>
2020-06-02 18:54:22 -0400

    Add .dir-locals.el to configure emacs.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((eval .
+               (progn
+                 (add-to-list 'exec-path
+                              (concat (locate-dominating-file default-directory dir-locals-file) "_opam/bin"))))))
+ )
