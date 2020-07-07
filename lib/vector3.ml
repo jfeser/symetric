b76e7675c9e98df8f5fb28ff67b3f58107c87ce9
Jack Feser <feser@csail.mit.edu>
2020-07-07 18:38:37 -0400

    Commit vector3.

---
+++ lib/vector3.ml
@@ -0,0 +1,24 @@
+open! Core
+
+module T = struct
+  type t = float * float * float [@@deriving compare, hash, sexp]
+end
+
+include T
+include Comparator.Make (T)
+
+let l2_dist (x, y, z) (x', y', z') =
+  Float.(sqrt ((x - x') ** 2.0) + ((y - y') ** 2.0) + ((z - z') ** 2.0))
+
+let inverse_rotate (rot_x, rot_y, rot_z) (x, y, z) =
+  let open Float in
+  let x1 = (cos (-rot_z) * x) - (sin (-rot_z) * y)
+  and y1 = (sin (-rot_z) * x) + (cos (-rot_z) * y)
+  and z1 = z in
+  let x2 = (cos (-rot_y) * x1) + (sin (-rot_y) * z1)
+  and y2 = y1
+  and z2 = (-sin (-rot_y) * x1) + (cos (-rot_y) * z1) in
+  let x3 = x2
+  and y3 = (cos (-rot_x) * y2) - (sin (-rot_x) * z2)
+  and z3 = (sin (-rot_x) * y2) + (cos (-rot_x) * z2) in
+  (x3, y3, z3)
