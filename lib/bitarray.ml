d7bcfb75bcdb42683d9c35209f975af23aae0903
Jack Feser <feser@csail.mit.edu>
2021-04-08 16:33:54 -0400

    Add tests for replicate.

--- lib/bitarray.ml
+++ lib/bitarray.ml
@@ -3,12 +3,14 @@ type t = { len : int; buf : string } [@@deriving compare, hash, sexp]
 let length t = t.len
 
 let init ~f len =
+  let nchars = (len / 8) + if len mod 8 > 0 then 1 else 0 in
   let buf =
-    String.init len ~f:(fun c ->
-        let i = c * 8 in
+    String.init nchars ~f:(fun c ->
         let char = ref 0 in
         for j = 0 to 7 do
-          if f (i + j) then char := !char + (1 lsl j)
+          let idx = (c * 8) + j in
+          let v = if idx < len && f idx then 1 else 0 in
+          char := !char + (v lsl j)
         done;
         char_of_int !char)
   in
@@ -19,6 +21,9 @@ let of_list x =
   init (Array.length x) ~f:(Array.get x)
 
 let get t i =
+  [%test_pred: int * int * int] ~message:"index out of bounds"
+    (fun (lo, x, hi) -> lo <= x && x < hi)
+    (0, i, length t);
   let ch = int_of_char t.buf.[i lsr 3] in
   let mask = 1 lsl (i land 7) in
   ch land mask <> 0
@@ -28,7 +33,9 @@ let to_list x = List.init (length x) ~f:(get x)
 let not a =
   {
     a with
-    buf = String.map a.buf ~f:(fun c -> char_of_int @@ lnot @@ int_of_char c);
+    buf =
+      String.map a.buf ~f:(fun c ->
+          char_of_int @@ ((lnot @@ int_of_char c) land 0xff));
   }
 
 let equal a b =
--- lib/cad_conc.ml
+++ lib/cad_conc.ml
@@ -19,8 +19,15 @@ let geti b = Bitarray.get b.pixels
 let replicate_is_set repl scene pt =
   let trans = Vector2.O.(-repl.Cad_op.v) in
   let rec loop count pt =
-    if count < 0 then false
-    else getp scene pt || loop (count - 1) Vector2.O.(pt + trans)
+    if count <= 0 then false
+    else
+      Float.O.(
+        pt.Vector2.x >= 0.0
+        && pt.x < of_int scene.xlen
+        && pt.y >= 0.0
+        && pt.y < of_int scene.ylen)
+      && getp scene pt
+      || loop (count - 1) Vector2.O.(pt + trans)
   in
   loop repl.count pt
 
@@ -34,20 +41,115 @@ let init params ~f =
   }
 
 let eval params op args =
-  match (op, args) with
-  | Cad_op.Inter, [ s; s' ] ->
-      init params ~f:(fun _ idx -> geti s idx && geti s' idx)
-  | Union, [ s; s' ] -> init params ~f:(fun _ idx -> geti s idx || geti s' idx)
-  | Circle c, [] ->
-      init params ~f:(fun pt _ ->
-          Float.(Vector2.(l2_dist c.center pt) <= c.radius))
-  | Rect r, [] ->
-      init params ~f:(fun k _ ->
-          Float.(
-            r.lo_left.x <= k.x && r.lo_left.y <= k.y && r.hi_right.x >= k.x
-            && r.hi_right.y >= k.y))
-  | Replicate r, [ s ] -> init params ~f:(fun pt _ -> replicate_is_set r s pt)
-  | _ -> raise_s [%message "Unexpected eval" (op : Cad_op.t)]
+  try
+    match (op, args) with
+    | Cad_op.Inter, [ s; s' ] ->
+        [%test_result: int] ~expect:(Bitarray.length s.pixels)
+          (Bitarray.length s'.pixels);
+        init params ~f:(fun _ idx -> geti s idx && geti s' idx)
+    | Union, [ s; s' ] ->
+        [%test_result: int] ~expect:(Bitarray.length s.pixels)
+          (Bitarray.length s'.pixels);
+        init params ~f:(fun _ idx -> geti s idx || geti s' idx)
+    | Circle c, [] ->
+        init params ~f:(fun pt _ ->
+            Float.(Vector2.(l2_dist c.center pt) <= c.radius))
+    | Rect r, [] ->
+        init params ~f:(fun k _ ->
+            Float.(
+              r.lo_left.x <= k.x && r.lo_left.y <= k.y && r.hi_right.x >= k.x
+              && r.hi_right.y >= k.y))
+    | Replicate r, [ s ] -> init params ~f:(fun pt _ -> replicate_is_set r s pt)
+    | _ -> raise_s [%message "Unexpected eval" (op : Cad_op.t)]
+  with e ->
+    raise Info.(to_exn @@ tag_s ~tag:[%message (op : Cad_op.t)] @@ of_exn e)
+
+let pprint fmt c =
+  for y = c.ylen - 1 downto 0 do
+    for x = 0 to c.xlen - 1 do
+      if getp c { x = Float.of_int x; y = Float.of_int y } then Fmt.pf fmt "X"
+      else Fmt.pf fmt "."
+    done;
+    Fmt.pf fmt "\n"
+  done
+
+let dummy_params ~xlen ~ylen =
+  Params.create
+    Cad_bench.
+      {
+        ops = [];
+        input = { xmax = xlen; ymax = ylen };
+        output =
+          { xlen = -1; ylen = -1; pixels = Bitarray.init 0 ~f:(fun _ -> false) };
+        solution = None;
+        filename = None;
+      }
+    Cad_params.{ concrete = false }
+
+let%expect_test "" =
+  let params = dummy_params ~xlen:8 ~ylen:8 in
+  eval params
+    (Circle { id = 0; center = { x = 3.0; y = 3.0 }; radius = 2.0 })
+    []
+  |> pprint Fmt.stdout;
+  [%expect
+    {|
+    ........
+    ........
+    ........
+    ..XXX...
+    .XXXXX..
+    ..XXX...
+    ........
+    ........ |}]
+
+let%expect_test "" =
+  let params = dummy_params ~xlen:8 ~ylen:8 in
+  eval params
+    (Rect
+       {
+         id = 0;
+         lo_left = { x = 1.0; y = 1.0 };
+         hi_right = { x = 4.0; y = 4.0 };
+       })
+    []
+  |> pprint Fmt.stdout;
+  [%expect
+    {|
+    ........
+    ........
+    ........
+    .XXXX...
+    .XXXX...
+    .XXXX...
+    .XXXX...
+    ........ |}]
+
+let%expect_test "" =
+  let params = dummy_params ~xlen:8 ~ylen:8 in
+  eval params
+    (Replicate { id = 0; count = 3; v = { x = 1.0; y = 1.0 } })
+    [
+      eval params
+        (Rect
+           {
+             id = 1;
+             lo_left = { x = 1.0; y = 1.0 };
+             hi_right = { x = 4.0; y = 4.0 };
+           })
+        [];
+    ]
+  |> pprint Fmt.stdout;
+  [%expect
+    {|
+    ........
+    ...XXXX.
+    ..XXXXX.
+    .XXXXXX.
+    .XXXXXX.
+    .XXXXX..
+    .XXXX...
+    ........ |}]
 
 let to_alist b =
   List.init (Bitarray.length b.pixels) ~f:(fun i -> (pt b i, geti b i))
