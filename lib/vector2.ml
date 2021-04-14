d2184e965aabe00aac232a075398c5587391fc56
Jack Feser <feser@csail.mit.edu>
2021-04-14 17:09:27 -0400

    Add vector2 operators.

--- lib/vector2.ml
+++ lib/vector2.ml
@@ -8,5 +8,13 @@ end
 include T
 include Comparator.Make (T)
 
+module O = struct
+  let ( + ) a b = { x = a.x +. b.x; y = a.y +. b.y }
+
+  let ( - ) a b = { x = a.x -. b.x; y = a.y -. b.y }
+
+  let ( ~- ) { x; y } = { x = -.x; y = -.y }
+end
+
 let l2_dist { x; y } { x = x'; y = y' } =
   Float.(sqrt ((x - x') ** 2.0) + ((y - y') ** 2.0))
