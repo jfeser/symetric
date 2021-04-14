ccf7590e907916be5d3522901e9732737531864a
Jack Feser <feser@csail.mit.edu>
2021-04-14 16:43:57 -0400

    Get last working version to build.

---
+++ lib/fresh.ml
@@ -0,0 +1,22 @@
+open! Core
+
+type t = int Hashtbl.M(String).t [@@deriving sexp]
+
+let create () = Hashtbl.create (module String)
+
+let name tbl fmt =
+  let key = string_of_format fmt in
+  (* if String.exists ~f:Char.is_digit key then
+   *   Error.create "Name template could collide." key [%sexp_of: string]
+   *   |> Error.raise ; *)
+  let ctr = Hashtbl.find tbl key |> Option.value ~default:0 in
+  let name = sprintf fmt ctr in
+  Hashtbl.set tbl ~key ~data:(ctr + 1);
+  name
+
+let reset = Hashtbl.clear
+
+let int tbl =
+  let ctr = Hashtbl.find tbl "" |> Option.value ~default:0 in
+  Hashtbl.set tbl ~key:"" ~data:(ctr + 1);
+  ctr
