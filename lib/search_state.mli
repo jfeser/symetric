cda3bbb790f7d5228bf80f1524e057eac8bc3bf7
Jack Feser <feser@csail.mit.edu>
2021-05-19 16:12:21 -0400

    Remove csg code, bool vector abstractions, lazy cegis impl.

--- lib/search_state.mli
+++
@@ -1,6 +0,0 @@
-module Make (Lang : Lang_abs_intf.S) :
-  Search_state_intf.S
-    with type op = Lang.Op.t
-     and type type_ = Lang.Type.t
-     and type abs = Lang.Abs.t
-     and type params = Lang.params
