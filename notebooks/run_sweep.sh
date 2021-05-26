8b2c7ded971664d24436c8027b5e8e79b6819980
Jack Feser <feser@csail.mit.edu>
2021-05-26 18:40:20 -0400

    Add git sha to output name.

--- notebooks/run_sweep.sh
+++ notebooks/run_sweep.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-NAME=sweep
+if [ -n "$(git status --porcelain)" ]; then 
+    echo "Working dir not clean";
+    exit 1
+fi
+
+NAME=sweep$(git rev-parse --short HEAD)
 WORKDIR=/home/feser/work/ocaml-workspace/staged-synth/
 BUILD_DIR="$WORKDIR/../_build/default/staged-synth/"
 OUT_FILE="$WORKDIR/notebooks/${NAME}_results.json"
 EXE=bin/lazy_cegis.exe
 FULL_EXE=$BUILD_DIR/$EXE
 
+cd $WORKDIR || exit
 dune build --profile=release "$EXE"
 
 find $WORKDIR/bench/cad2/random_repl_size_11/ -name "*.sexp" | sort | head -n10 > $WORKDIR/jobs
@@ -16,3 +22,4 @@ parallel --eta --joblog "$WORKDIR/notebooks/${NAME}_joblog" --timeout 600 --cols
          "$FULL_EXE cad-sample-diverse -max-cost 11 -print-json true -d 0.0 -w 0 -p {2} -seed {1} -diversity {3} {4} 2> /dev/null" \
          ::: {0..99} ::: 50 ::: true false :::: $WORKDIR/jobs \
     >> "$OUT_FILE"
+rm -f $WORKDIR/jobs
