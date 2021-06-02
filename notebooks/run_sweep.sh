99d5560aa24dd78bc827f2ac5dd31284fd8ef072
Jack Feser <feser@csail.mit.edu>
2021-06-02 16:15:14 -0400

    Prep for benchmark.

--- notebooks/run_sweep.sh
+++ notebooks/run_sweep.sh
@@ -15,11 +15,18 @@ FULL_EXE=$BUILD_DIR/$EXE
 cd $WORKDIR || exit
 dune build --profile=release "$EXE"
 
-find $WORKDIR/bench/cad2/random_repl_size_11/ -name "*.sexp" | sort | head -n10 > $WORKDIR/jobs
+find $WORKDIR/bench/cad2/random_repl_size_11/ -name "*.sexp" | sort | head -n30 > $WORKDIR/jobs
 
 rm -f "$OUT_FILE"
+
 parallel --eta --joblog "$WORKDIR/notebooks/${NAME}_joblog" --timeout 600 --colsep ' ' \
-         "$FULL_EXE cad-sample-diverse -max-cost 11 -print-json true -d 0.0 -w 0 -p {2} -seed {1} -diversity {3} {4} 2> /dev/null" \
+         "$FULL_EXE cad-sample-diverse -max-cost 11 -print-json true -d 0.1 -w 5 -p {2} -seed {1} -diversity {3} {4} 2> /dev/null" \
          ::: {0..99} ::: 50 ::: true false :::: $WORKDIR/jobs \
-    >> "$OUT_FILE"
+         >> "$OUT_FILE"
+
+parallel --eta --joblog "$WORKDIR/notebooks/${NAME}_joblog" --timeout 600 --colsep ' ' \
+         "$FULL_EXE cad-baseline -max-cost 11 -print-json true {4} 2> /dev/null" \
+         :::: $WORKDIR/jobs \
+         >> "$OUT_FILE"
+
 rm -f $WORKDIR/jobs
