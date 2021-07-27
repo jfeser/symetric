e3975cf3ce2092dfd71f99cda750fb4fec19438c
Jack Feser <feser@csail.mit.edu>
2021-07-27 16:21:51 -0400

    Fiddle with metric learning.

--- lib/learned_cost.ml
+++ lib/learned_cost.ml
@@ -11,13 +11,15 @@ include struct
   let inter_centroid_distance =
     Spec.add spec
     @@ Param.float ~name:"inter-centroid-distance" ~doc:" target distance between cluster centers"
-         ~init:(`Cli (Some 1.0)) ()
+         ~init:(`Cli (Some 200.0)) ()
 end
 
 module Lang = Cad
 module Parent = Sampling_diverse.Make (Lang)
 module Search_state = Search_state_append.Make (Lang)
 module Gen = Synth_utils.Generate_list (Lang)
+
+exception Reset
 
 class synthesizer params =
   let open Lang in
@@ -30,46 +32,58 @@ class synthesizer params =
 
     val mutable metric = Metric_learn.Lego.create 900
 
-    val mutable centers = []
+    val mutable centers = Map.empty (module Value)
+
+    method to_features v =
+      let module Mat = Owl.Mat in
+      let module Arr = Owl.Arr in
+      Mat.transpose @@ Arr.expand (Value.to_ndarray v) 2
 
     method! dist v v' =
       let module Mat = Owl.Mat in
-      let module Arr = Owl.Arr in
       try
-        let v = Mat.transpose @@ Arr.expand (Value.to_ndarray v) 2
-        and v' = Mat.transpose @@ Arr.expand (Value.to_ndarray v') 2 in
-        Mat.((Metric_learn.Lego.dist metric v v').%{(0, 0)})
+        let v = self#to_features v and v' = self#to_features v' in
+        let d = Metric_learn.Lego.dist metric v v' in
+        d
       with exn ->
         print_string (Owl_exception.to_string exn);
         raise exn
 
     method update_metric center neighbors =
       let module Mat = Owl.Mat in
-      let center = Value.to_ndarray center and neighbors = List.map neighbors ~f:Value.to_ndarray in
-      let n_close = List.length neighbors and n_far = List.length centers in
-      let lhs = Array.init (n_close + n_far) ~f:(Fun.const center) |> Mat.of_rows
-      and rhs = Array.of_list (neighbors @ centers) |> Mat.of_rows
-      and dists =
-        let dists_arr =
-          Array.of_list @@ List.init n_close ~f:(Fun.const 0.0) @ List.init n_far ~f:(Fun.const inter_centroid_distance)
-        in
-        Mat.of_array dists_arr (Array.length dists_arr) 1
+      let center_m = self#to_features center and neighbors_m = List.map neighbors ~f:self#to_features in
+
+      let metric' = List.fold_left ~init:metric ~f:(fun m n -> Metric_learn.Lego.update m center_m n 0.0) neighbors_m in
+      let metric' =
+        Map.fold ~init:metric'
+          ~f:(fun ~key:v ~data:c m ->
+            if [%compare.equal: Value.t] v center then m
+            else Metric_learn.Lego.update m center_m c inter_centroid_distance)
+          centers
       in
-      Mat.print lhs;
-      metric <- Metric_learn.Lego.update metric lhs rhs dists;
-      centers <- center :: centers
+
+      (* Fmt.pr "Center:\n%a\n" Value.pprint center;
+       * List.iter neighbors ~f:(Fmt.pr "Neighbor:\n%a\n" Value.pprint);
+       * Map.to_alist centers |> List.iter ~f:(fun (v, _) -> Fmt.pr "Alt center:\n%a\n" Value.pprint v); *)
+      metric <- metric';
+      centers <- Map.update centers center ~f:(function Some c -> c | None -> center_m)
 
     method! search_neighbors ?(view = ignore) center f =
       print_s [%message "searching..."];
       let sampler = Sample.Incremental.reservoir_unique (module Value) neighbor_sample_size in
+      let center_v = Program.eval (Value.eval params) center in
+
+      Fmt.pr "Center:\n%a\n" Value.pprint center_v;
+
       super#search_neighbors center
         ~view:(fun v ->
           view v;
-          sampler.Sample.Incremental.add v)
+          if not @@ [%compare.equal: Value.t] v center_v then sampler.Sample.Incremental.add v)
         f;
       let neighbors = sampler.get_sample () in
-      let center = Program.eval (Value.eval params) center in
-      self#update_metric center neighbors
+      self#update_metric center_v neighbors
+
+    method! run = try super#run with Reset -> self#run
   end
 
 let synth params = Option.iter (new synthesizer params)#run ~f:(fun p -> eprint_s [%message (p : Lang.Op.t Program.t)])
--- lib/metric_learn.ml
+++ lib/metric_learn.ml
@@ -1,55 +1,115 @@
 open Owl
 
+module Mat = struct
+  include Mat
+
+  let sexp_of_mat m = [%sexp_of: float array array] @@ to_arrays m
+end
+
 module Lego = struct
   type t = { dist : Mat.mat; (* Distance matrix (DxD) *) eta : float (* Regularization parameter (> 0) *) }
 
   let create ?(eta = 1.0) n_features = { dist = Mat.eye n_features; eta }
 
+  (** Compute the distance between feature vectors.
+      @param u, v : F x 1
+      @return scalar distance *)
   let dist x u v =
     let z = Mat.(u - v) in
-    Mat.(transpose z *@ x.dist *@ z)
+    Mat.((transpose z *@ x.dist *@ z).%{(0, 0)})
 
+  (** Update the distance metric
+      @param u, v : Fx1
+      @param y : scalar
+      @return New distance metric *)
   let update x u v y =
     let z = Mat.(u - v) in
-    let y_est = Mat.(transpose z *@ x.dist *@ z) in
+    let y_est = Mat.((transpose z *@ x.dist *@ z).%{(0, 0)}) in
     let y_bar =
-      Mat.(
-        let b = x.eta $* y_est in
-        let a = (y * b) -$ 1.0 in
-        let num = a + sqrt ((a * a) + (4.0 $* (x.eta $* y_est * y_est))) in
-        let dem = 2.0 $* b in
+      Float.(
+        let b = x.eta * y_est in
+        let a = (y * b) - 1.0 in
+        let num = a + sqrt ((a * a) + (4.0 * (x.eta * y_est * y_est))) in
+        let dem = 2.0 * b in
         num / dem)
     in
-    let y_diff =
-      Mat.(
-        let d = y_bar - y in
-        x.eta $* d)
-    in
+    let y_diff = Float.(x.eta * (y_bar - y)) in
     let dist' =
       Mat.(
-        let num = y_diff * (x.dist *@ z *@ transpose z *@ x.dist) in
-        let dem = 1.0 $+ y_diff * y_est in
-        x.dist - (num / dem))
+        let num = y_diff $* x.dist *@ z *@ transpose z *@ x.dist in
+        let dem = Float.(1.0 + (y_diff * y_est)) in
+        x.dist - (num /$ dem))
     in
-    { x with dist = dist' }
+    if Mat.not_nan dist' && Mat.not_inf dist' then { x with dist = dist' }
+    else (
+      Mat.print ~max_row:100 ~max_col:20 x.dist;
+      failwith "bad update")
 
   let%expect_test "" =
     try
       let x = create 4 in
+      Mat.print x.dist;
+      print_s [%message (Mat.is_normal x.dist : bool)];
+
       let c1 = Mat.of_array [| 0.0; 1.0; 0.0; 0.0 |] 4 1
       and n1 = Mat.of_array [| 1.0; 0.0; 0.0; 0.0 |] 4 1
       and c2 = Mat.of_array [| 0.0; 0.0; 0.0; 1.0 |] 4 1
       and n2 = Mat.of_array [| 0.0; 0.0; 1.0; 0.0 |] 4 1 in
 
-      let x = update x c1 n1 (Mat.of_array [| 0.0 |] 1 1) in
-      let x = update x c2 n2 (Mat.of_array [| 0.0 |] 1 1) in
-      let x = update x c1 c2 (Mat.of_array [| 1.0 |] 1 1) in
+      let x = update x c1 n1 0.0 in
+      Mat.print x.dist;
+      print_s [%message (Mat.is_normal x.dist : bool)];
+      let x = update x c2 n2 0.0 in
+      Mat.print x.dist;
+      print_s [%message (Mat.is_normal x.dist : bool)];
+      let x = update x c1 c2 1.0 in
+      Mat.print x.dist;
+      print_s [%message (Mat.is_normal x.dist : bool)];
 
-      Fmt.pr "c1 n1\n%a\n" Owl_pretty.pp_dsnda @@ dist x c1 n1;
-      Fmt.pr "c1 n2\n%a\n" Owl_pretty.pp_dsnda @@ dist x c1 n2;
-      Fmt.pr "c1 c2\n%a\n" Owl_pretty.pp_dsnda @@ dist x c1 c2;
-      Fmt.pr "n1 n2\n%a\n" Owl_pretty.pp_dsnda @@ dist x n1 n2;
-      Fmt.pr "n1 c2\n%a\n" Owl_pretty.pp_dsnda @@ dist x n1 c2;
-      Fmt.pr "n2 c2\n%a\n" Owl_pretty.pp_dsnda @@ dist x n2 c2
-    with exn -> print_string (Owl_exception.to_string exn)
+      Fmt.pr "c1 n1 %f\n" @@ dist x c1 n1;
+      Fmt.pr "c1 n2 %f\n" @@ dist x c1 n2;
+      Fmt.pr "c1 c2 %f\n" @@ dist x c1 c2;
+      Fmt.pr "n1 n2 %f\n" @@ dist x n1 n2;
+      Fmt.pr "n1 c2 %f\n" @@ dist x n1 c2;
+      Fmt.pr "n2 c2 %f\n" @@ dist x n2 c2
+    with exn ->
+      print_string (Owl_exception.to_string exn);
+      [%expect
+        {|
+         C0 C1 C2 C3
+      R0  1  0  0  0
+      R1  0  1  0  0
+      R2  0  0  1  0
+      R3  0  0  0  1
+      ("Mat.is_normal x.dist" false)
+
+
+               C0       C1 C2 C3
+      R0 0.695194 0.304806  0  0
+      R1 0.304806 0.695194  0  0
+      R2        0        0  1  0
+      R3        0        0  0  1
+      ("Mat.is_normal x.dist" false)
+
+
+               C0       C1       C2       C3
+      R0 0.695194 0.304806        0        0
+      R1 0.304806 0.695194        0        0
+      R2        0        0 0.695194 0.304806
+      R3        0        0 0.304806 0.695194
+      ("Mat.is_normal x.dist" false)
+
+
+                C0        C1        C2        C3
+      R0  0.683651  0.278478 0.0115435 0.0263281
+      R1  0.278478  0.635146 0.0263281 0.0600484
+      R2 0.0115435 0.0263281  0.683651  0.278478
+      R3 0.0263281 0.0600484  0.278478  0.635146
+      ("Mat.is_normal x.dist" true)
+      c1 n1 0.761841
+      c1 n2 1.266140
+      c1 c2 1.150195
+      n1 n2 1.344214
+      n1 c2 1.266140
+      n2 c2 0.761841 |}]
 end
