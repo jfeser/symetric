b3a6016da17ad0735683e78c6fb4c67118109b4e
Jack Feser <feser@csail.mit.edu>
2022-01-18 11:15:06 -0500

    add scaling to print bench

--- bin/print_bench.ml
+++ bin/print_bench.ml
@@ -10,13 +10,15 @@ let () =
         flag "-scene-width" (optional_with_default 12 int) ~doc:" scene width in pixels"
       and scene_height =
         flag "-scene-height" (optional_with_default 20 int) ~doc:" scene height in pixels"
+      and scaling =
+        flag "-scaling" (optional_with_default 1 int) ~doc:" scene scaling factor"
       in
       fun () ->
         let ectx =
-          Value.Ctx.create @@ Scene2d.Dim.create ~xres:scene_width ~yres:scene_height ()
+          Value.Ctx.create
+          @@ Scene2d.Dim.create ~xres:scene_width ~yres:scene_height ~scaling ()
         in
-        Fmt.pr "%a\n" Value.pp
-        @@ Program.eval (Value.eval ectx)
-        @@ parse
-        @@ Sexp.input_sexp In_channel.stdin]
+        let p = parse @@ Sexp.input_sexp In_channel.stdin in
+        Fmt.pr "cost=%d\n" (Program.size p);
+        Fmt.pr "%a\n" Value.pp @@ Program.eval (Value.eval ectx) p]
   |> Command.run
