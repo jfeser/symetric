b1470d0e7172971acc53a4d25e82345c974060bb
Jack Feser <feser@mit.edu>
2022-04-10 22:43:23 -0400

    fix up exhaustive search

--- lib/enumerate_cad.ml
+++ lib/enumerate_cad.ml
@@ -8,8 +8,6 @@ module Params = struct
     ectx : Value.Ctx.t;
     target : Scene2d.t;
     operators : Op.t list;
-    filter : bool;
-    max_cost : int;
     verbose : bool;
   }
 end
@@ -19,8 +17,6 @@ let[@inline] size () = (Set_once.get_exn params [%here]).Params.size
 let[@inline] ectx () = (Set_once.get_exn params [%here]).Params.ectx
 let[@inline] target () = (Set_once.get_exn params [%here]).Params.target
 let[@inline] operators () = (Set_once.get_exn params [%here]).Params.operators
-let[@inline] filter () = (Set_once.get_exn params [%here]).Params.filter
-let[@inline] max_cost () = (Set_once.get_exn params [%here]).Params.max_cost
 let[@inline] verbose () = (Set_once.get_exn params [%here]).Params.verbose
 let runtime = ref Time.Span.zero
 
@@ -30,11 +26,8 @@ let synthesize () =
   let ectx = ectx ()
   and operators = operators ()
   and target = target ()
-  and verbose = verbose ()
-  and max_cost = max_cost () in
-  let ctx =
-    Synth.Ctx.create ~verbose ~max_cost ectx operators (`Value (Value.Scene target))
-  in
+  and verbose = verbose () in
+  let ctx = Synth.Ctx.create ~verbose ectx operators (`Value (Value.Scene target)) in
   let synth = new Synth.synthesizer ctx in
   let ret = synth#run in
   runtime := Time.diff (Time.now ()) start_time;
@@ -56,45 +49,27 @@ let print_output m_prog =
         ("method", `String "enumeration");
         ("scene_width", `Int (size ()).xres);
         ("scene_height", `Int (size ()).yres);
-        ("filter", `Bool (filter ()));
-        ("max_cost", `Int (max_cost ()));
         ("program_size", `Float program_size);
         ("runtime", `Float (Time.Span.to_sec !runtime));
         ("program", program_json);
       ])
 
-let set_params ~scene_width ~scene_height ~max_cost ~filter ~verbose target =
-  let size = Scene2d.Dim.create ~xres:scene_width ~yres:scene_height () in
+let set_params ~dim:size ~verbose target =
   let ectx = Value.Ctx.create size in
   let target_value = Program.eval (Value.eval ectx) target in
   let target_scene = match target_value with Scene s -> s | _ -> assert false in
-  let operators =
-    Op.[ Union; Circle; Rect; Repl; Sub ]
-    @ (List.range 0 (max size.xres size.yres) |> List.map ~f:(fun i -> Op.Int i))
-    @ (List.range 2 5 |> List.map ~f:(fun i -> Op.Rep_count i))
-  in
+  let operators = Op.default_operators ~xres:size.xres ~yres:size.yres in
 
   Set_once.set_exn params [%here]
-    Params.{ size; ectx; target = target_scene; operators; max_cost; filter; verbose }
+    Params.{ size; ectx; target = target_scene; operators; verbose }
 
 let cmd =
   let open Command.Let_syntax in
   Command.basic ~summary:"Solve CAD problems with enumeration."
     [%map_open
-      let max_cost =
-        flag "-max-cost"
-          (optional_with_default 22 int)
-          ~doc:" the maximum size of program to evaluate"
-      and filter =
-        flag "-use-filter"
-          (optional_with_default true bool)
-          ~doc:" use heuristic filtering"
-      and scene_width =
-        flag "-scene-width" (optional_with_default 12 int) ~doc:" scene width in pixels"
-      and scene_height =
-        flag "-scene-height" (optional_with_default 20 int) ~doc:" scene height in pixels"
+      let dim = Scene2d.Dim.param
       and verbose = flag "-verbose" no_arg ~doc:" increase verbosity" in
       fun () ->
         let prog = Cad_ext.parse @@ Sexp.input_sexp In_channel.stdin in
-        set_params ~scene_width ~scene_height ~max_cost ~filter ~verbose prog;
+        set_params ~dim ~verbose prog;
         synthesize () |> print_output]
